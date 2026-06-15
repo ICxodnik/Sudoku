6d5f1af5f32247d30922776be26e885c7cbad6d2
kit <kit@DESKTOP-071MFQD>
2026-06-15 16:53:44 +0200

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Common;
+
+public class Solution
+{
+    public static void Main()
+    {
+        char[][] board = [
+            ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+            ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+        ];
+        var result = IsValidSudoku(board);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+    public static bool IsValidSudoku(char[][] board)
+    {
+        //vaidate rows
+        for (int i = 0; i < board.Length - 1; i++)
+        {
+            if (IsValidRow(board[i]) == false)
+            {
+                Console.WriteLine("Dublicate in row " + i);
+                return false;
+            }
+
+            //validate columns
+            char[] column = new char[board[i].Length];
+            for (int j = 0; j <= board[i].Length - 1; j++)
+            {
+                column[j] = board[j][i];
+            }
+            if (IsValidRow(column) == false)
+            {
+                Console.WriteLine("Dublicate in column " + i);
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public static bool IsValidRow(char[] row)
+    {
+        char[] array = (char[])row.Clone();
+        array.Sort();
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == '.')
+            {
+                continue;
+            }
+            if (array[i] == array[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // public bool IsValidSubBoxes(char[]column) {
+
+    // }
+}
